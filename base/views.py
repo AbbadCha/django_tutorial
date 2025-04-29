18547fc713957defaba81cf8f4cba1cb6eb86197
Abbad Chamma <abbadchamma@gmail.com>
2025-04-29 14:30:00 -0400

    Set up basic views and URLs for home and room pages

--- base/views.py
+++ base/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    context = {'page': 'home'}
+    return render(request, 'base/home.html', context)
+
+def room(request, pk):
+    context = {'page': 'room', 'room_id': pk}
+    return render(request, 'base/room.html', context)
